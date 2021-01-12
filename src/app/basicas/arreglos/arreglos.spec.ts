71877bd425afea70eaf7a3d26751e0ff4f02bf3c
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-01-11 23:54:16 -0400

    Pruebas con arreglos

---
+++ src/app/basicas/arreglos/arreglos.spec.ts
@@ -0,0 +1,13 @@
+import { obtenerRobots } from "./arreglos"
+
+describe('Pruebas de de arreglos', () => {
+    it('Debe de retornar al menos 3 robots',()=>{
+        const res = obtenerRobots();
+        expect(res.length).toBeGreaterThanOrEqual(3);
+    })
+    it('Debe de existir Megaman y Ultron',()=>{
+        const res = obtenerRobots();
+        expect(res).toContain('Megaman');
+        expect(res).toContain('Ultron');
+    })
+})
