a4d902169a9f71f19880c9020df24bf71b398010
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-01-12 22:31:48 -0400

    Uso de 'beforeEach' para restablecer el valor jugador por cada prueba

--- src/app/basicas/clases/clase.spec.ts
+++ src/app/basicas/clases/clase.spec.ts
@@ -1,5 +1,18 @@
 import { Jugador } from "./clase"
-const jugador= new Jugador();
+let jugador= new Jugador();
+beforeAll(()=>{
+    // console.log('beforeAll');
+});
+beforeEach(()=>{
+    // console.log('beforeEach');
+    jugador = new Jugador();
+});
+afterAll(()=>{
+    // console.log('AfterAll');
+});
+afterEach(()=>{
+    // console.log('AfterEach');
+});
 describe('Pruebas de clase', () => {
     it('Debe de retornar 80 de hp, si recibe 20 de daño',()=>{
         const resp =  jugador.recibeDanio(20);
@@ -7,6 +20,10 @@ describe('Pruebas de clase', () => {
     })
     it('Debe de retornar 50 de hp, si recibe 50 de daño',()=>{
         const resp =  jugador.recibeDanio(50);
-        expect(resp).toBe(30);
+        expect(resp).toBe(50);
+    })
+    it('Debe de retornar 0 de hp, si recibe 100 de daño o más',()=>{
+        const resp =  jugador.recibeDanio(100);
+        expect(resp).toBe(0);
     })
 })
