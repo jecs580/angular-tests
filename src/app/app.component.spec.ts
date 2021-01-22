c7a0fea542715d56287d835280e837b2f83c7973
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-01-21 23:19:49 -0400

    Prueba para verificar que exista un routerLink para 'medicos'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing'
 import { By } from '@angular/platform-browser';
-import { RouterOutlet } from '@angular/router';
+import { RouterLinkWithHref, RouterOutlet } from '@angular/router';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -38,5 +38,19 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const debugElement = fixture.debugElement.query(By.directive(RouterOutlet));
     expect(debugElement).not.toBeNull();
+  })
+
+  it('Debe de tener un link a la página de médicos',()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const elementos= fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+    
+    let existe = false;
+    for (const elem of elementos) {
+      if (elem.attributes['routerLink']==='/medicos') {
+        existe = true;
+        break;
+      }
+    }
+    expect(existe).toBeTruthy();
   })
 });
